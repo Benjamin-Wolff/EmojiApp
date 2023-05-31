5df3b9826ae3901e982e8df0bb323848acec350a
Benjamin-Wolff <wolff.benjamind@gmail.com>
2023-05-31 13:42:24 -0400

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Benjamin Wolff on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
